6771023c3e61bb5a3128af536276ef0059cadc69
Alejandro Cortes <ale.crts1996@gmail.com>
2016-08-31 02:24:03 +0000

    - Added body for AST class and header.

---
+++ Project1/AST.cpp
@@ -0,0 +1,16 @@
+#include "AST.h"
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Generic constructor.
+AST::AST(){
+    
+}
+
+// AST with a postfix string input.
+AST::AST(string AST) {
+    
+}
---
+++ Project1/AST.h
@@ -0,0 +1,20 @@
+//#pragma once
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class AST
+{
+public:
+    // Basic constructor
+    AST();
+    // AST with an input postfix string
+    AST(string postfix);
+private:
+    // We need a tree-like to build our AST
+    
+    // We need a stack to hold the nodes that we will generate
+    
+}
